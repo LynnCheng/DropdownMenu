58962b64bd76abab49d75db98661a54349fc8ed4
Lynn <77708550@qq.com>
2019-07-01 12:00:20 +0800

    modify

--- UIViewExtensions.swift
+++ UIViewExtensions.swift
@@ -8,65 +8,6 @@
 
 import UIKit
 
-public extension UIView {
-    var screenshot:UIImage? {
-        UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, 0)
-        defer {
-            UIGraphicsEndImageContext()
-        }
-        
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return nil
-        }
-        
-        layer.render(in: context)
-        return UIGraphicsGetImageFromCurrentImageContext()
-    }
-    
-    func parentViewController() -> UIViewController? {
-        weak var parentResponder: UIResponder? = self
-        while parentResponder != nil {
-            parentResponder = parentResponder!.next
-            if let viewController = parentResponder as? UIViewController {
-                return viewController
-            }
-        }
-        return nil
-    }
-    
-    func addSubviews(_ subviews: UIView...) {
-        subviews.forEach(addSubview)
-    }
-    
-    func removeSubviews() {
-        for subview in self.subviews {
-            subview.removeFromSuperview()
-        }
-    }
-}
-
-extension UIView {
-    func fadeTransition(_ duration: CFTimeInterval) {
-        let animation = CATransition()
-        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
-        animation.type = .fade
-        animation.duration = duration
-        layer.add(animation, forKey: "fade")  //forkey?
-    }
-}
-
-extension UIView {
-    @IBInspectable public var cornerRadius: CGFloat {
-        get {
-            return layer.cornerRadius
-        }
-        set {
-            layer.masksToBounds = true
-            layer.cornerRadius = abs(CGFloat(Int(newValue * 100)) / 100)
-        }
-    }
-}
-
 extension UIView {
     func setAnchorPoint(_ point: CGPoint) {
         var newPoint = CGPoint(x: bounds.size.width * point.x, y: bounds.size.height * point.y)
